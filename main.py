c866fd57df745d235811712885ff0baf9f9e1f7e
ventiscar34-eng <ventiscar34@gmail.com>
2026-08-27 13:30:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
+
+# --- โค้ดบอท Discord เดิมของคุณวางต่อตรงนี้ ---
